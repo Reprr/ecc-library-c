39521adb3e832c90ea982e4571a32363c1349dce
Lev <lev.zargarov@yandex.ru>
2026-08-13 19:48:32 +0300

    fixed some bugs in serialization of shared secret

--- src/serialize.c
+++ src/serialize.c
@@ -73,26 +73,21 @@ ecc_status_code serialize_priv_key_sec1(
     return ECC_OK;
 }
 
-ecc_status_code serialize_shared_priv_key_sec1(
-    const ecc_shared_private_key *shared_pr_k,
-    serialized_shared_priv_key *out,
+ecc_status_code serialize_priv_key_sec1(
+    const ecc_private_key *pr_k,
+    serialized_priv_key *out,
     const ecc_curve *curve
 ) {
-    if (!shared_pr_k || !shared_pr_k->G || !out || !curve) return ECC_INVALID_PARAMS;
+    if (!pr_k || !pr_k->n || !out || !curve) return ECC_INVALID_PARAMS;
+
+    if (cmp(*pr_k->n, from_u64(1)) < 0 || cmp(*pr_k->n, curve->N) >= 0)
+        return ECC_INVALID_PARAMS;
 
     memset(out->data, 0, sizeof(out->data));
-    if (shared_pr_k->G->inf || equal(shared_pr_k->G->z, from_u64(0))) {
-        out->data[0] = 0x00;
-        return ECC_INVALID_PARAMS;
-    }
 
-    ecc_point_affine P_aff;
-    projective_to_affine(&P_aff, shared_pr_k->G, curve->F->p);
-    if (!is_on_curve(&P_aff, curve)) return ECC_INVALID_PARAMS;
-
-    for (int i = NUM_LIMBS-1, idx = 0; i >= 0; --i) {
+    for (int i = NUM_LIMBS - 1, idx = 0; i >= 0; --i) {
         for (int j = 3; j >= 0; --j, ++idx) {
-            out->data[idx] =  ((P_aff.x.d[i] >> (8 * j)) & 0xFF);
+            out->data[idx] = (pr_k->n->d[i] >> (8 * j)) & 0xFF;
         }
     }
     return ECC_OK;
